2c2dc90546c67afd978cb6d34e8ce7413d1b840a
RubiKarp <a.deleye@rubika-edu.com>
2019-10-05 23:18:03 +0200

    Je fais des tests

--- PROTO-3-RogueLike_TheHand/Assets/_Script/Ennemis/ennemisRush.cs
+++ PROTO-3-RogueLike_TheHand/Assets/_Script/Ennemis/ennemisRush.cs
@@ -7,15 +7,21 @@ public class ennemisRush : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private Transform mySelf;
     [SerializeField] private float speed;
-    [SerializeField] private float detectingDistance;
+    [SerializeField] private float detectingDistance, tropProche;
 
     private void Update()
     {
-        //Si la cible est assez proche
-        if (Vector2.Distance(transform.position, target.position) < detectingDistance)
+        //Si la cible est assez proche pour etre detect mais pas tropProche non plus 
+        if (detectingDistance > Vector2.Distance(transform.position, target.position) && tropProche < Vector2.Distance(transform.position, target.position))
         {
             //Rush B !!! go ! go ! go !
             mySelf.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+
+        if(tropProche < Vector2.Distance(transform.position, target.position))
+        {
+            //attaque
         }
     }
 }
